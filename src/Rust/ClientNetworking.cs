35cb92012e4a1a8288015f25dba9eaa5e09362f4
raul <raulssorban@gmail.com>
2024-10-18 23:37:17 +0200

    Whoops

---
+++ src/Rust/ClientNetworking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Carbon;
+
+public static unsafe class ClientNetworking
+{
+	public static void SendMessage(ReadOnlySpan<byte> data, int priority, int reliability, int channel)
+	{
+		fixed (byte* be = data)
+		{
+			send_message(be, data.Length, priority, reliability, channel);
+		}
+	}
+
+	[DllImport("Carbon_Client_Native", CallingConvention = CallingConvention.Cdecl)]
+	public static extern void send_message(byte* bytes, int len, int priority, int reliability, int channel);
+
+	public static void OnMesage(byte* ptr, int len)
+	{
+		ReadOnlySpan<byte> data = new(ptr, len);
+		// do stuff with data
+
+		Debug.Log($"brr {data.Length}");
+	}
+
+	public static void Init()
+	{
+		Debug.Log($"A) {LoadLibrary(Path.Combine(Application.dataPath, "Plugins", "x86_64", "RakNet.dll"))}");
+		Debug.Log($"B) {LoadLibrary(Path.Combine(Application.dataPath, "Plugins", "x86_64", "EOSSDK-Win64-Shipping.dll"))}");
+
+		init(&OnMesage);
+	}
+
+	[DllImport("Carbon_Client_Native", CallingConvention = CallingConvention.Cdecl)]
+	public static extern void init(delegate*<byte*, int, void> callback);
+
+	[DllImport("kernel32.dll")]
+	public static extern IntPtr LoadLibrary(string dllToLoad);
+}
---
+++ src/Rust/Rust.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyRoads3Dv3;
+using UnityEngine;
+
+namespace Carbon;
+
+public class Rust
+{
+	public static Dictionary<string, GameObject> prefabs = new();
+
+    public static GameObjectCache MenuUI = new("MenuUI(Clone)");
+    public static GameObjectCache EngineUI = new("EngineUI(Clone)");
+
+    public static Action OnMenuShow;
+    public static Action OnMenuHide;
+
+	public static GameObject FindPrefab(string name)
+	{
+		if(prefabs.TryGetValue(name, out var prefab))
+		{
+			return prefab;
+		}
+
+		var bundles = AssetBundle.GetAllLoadedAssetBundles_Native();
+
+		foreach (var bundle in bundles)
+		{
+			var asset = bundle.LoadAsset<GameObject>(name);
+
+			if (asset != null)
+			{
+				prefabs[name] = asset;
+				return asset;
+			}
+		}
+
+		return null;
+	}
+	public static GameObject SpawnPrefab(string name, Vector3 pos = default, Quaternion rot = default, Vector3 scale = default)
+	{
+		var source = FindPrefab(name);
+
+		if(source == null)
+		{
+			return null;
+		}
+
+		var instance = GameObject.Instantiate(source);
+		instance.transform.SetPositionAndRotation(pos, rot);
+
+		if(scale != default)
+		{
+			instance.transform.localScale = scale;
+		}
+
+		return instance;
+	}
+}
